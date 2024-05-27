4f2f383ac03ac189a947efbe1e887dacdcb1b6d2
DTAJ095 <albanjaures26@gmail.com>
2024-05-27 12:24:13 -0700

    added

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,13 +2,13 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-/* eslint-disable unused variables */
+/* eslint-disable no-unused-variables */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
 
-/* eslint-disable unused variables */
+/* eslint-disable no-unused-variables */
 
   return [task, task2];
 }
