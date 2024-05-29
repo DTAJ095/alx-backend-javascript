5c94928f297816aefcbc70d39224a160eb496908
DTAJ095 <albanjaures26@gmail.com>
2024-05-29 10:20:50 -0700

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,27 +1,27 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-	const response = [];
+export default async function handleProfileSignup (firstName, lastName, fileName) {
+  const response = [];
   try {
     const user = await signUpUser(firstName, lastName);
-	  response.push({ status: 'Completed', value: user });
+    response.push({ status: 'Completed', value: user });
   } catch (err) {
-	  response.push({
-		  status: 'Rejected',
-		  value: err.toString(),
-	  });
+    response.push({
+      status: 'Rejected',
+      value: err.toString(),
+    });
   }
 
-	try {
-		const upload = await uploadPhoto(filename);
-		response.push({ status: 'Completed', value: upload });
-	} catch (err) {
-		response.push({
-			status: 'Rejected',
-			value: err.toString(),
-		});
-	}
+  try {
+    const upload = await uploadPhoto(filename);
+    response.push({ status: 'Completed', value: upload });
+  } catch (err) {
+    response.push({
+      status: 'Rejected',
+      value: err.toString(),
+    });
+  }
 
-	return response;
+  return response;
 }
