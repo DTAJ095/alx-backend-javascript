3efa48a1a4cb9b4dc9e83106a3c4d039536a959e
DTAJ095 <albanjaures26@gmail.com>
2024-05-29 10:23:43 -0700

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup (firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const response = [];
   try {
     const user = await signUpUser(firstName, lastName);
