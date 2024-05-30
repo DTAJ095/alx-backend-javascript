1e7179f470b0a9fc8eeae54341fa090b9148c581
DTAJ095 <albanjaures26@gmail.com>
2024-05-30 16:17:35 -0700

    added

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,21 +1,21 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        if (Object.getPrototypeOf(size) !== Number.prototype) {
-            throw TypeError('Size must be a number');
-        }
-        if (Object.getPrototypeOf(location) !== String.prototype) {
-            throw TypeError('Location must be a string');
-        }
-        this._size = size;
-        this._location = location;
+  constructor(size, location) {
+    if (Object.getPrototypeOf(size) !== Number.prototype) {
+      throw TypeError('Size must be a number');
     }
+    if (Object.getPrototypeOf(location) !== String.prototype) {
+      throw TypeError('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
 
-    // Method
-    [Symbol.toPrimitive](type) {
-        if (type === 'string') {
-            return this._location;
-        } else {
-            return this._size;
-        }
+  // Method
+  [Symbol.toPrimitive](type) {
+    if (type === 'string') {
+      return this._location;
+    } else {
+      return this._size;
     }
+  }
 }
