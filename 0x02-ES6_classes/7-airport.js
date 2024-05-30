564722977819c49c0ca7ebcc7762eea70e23eb51
DTAJ095 <albanjaures26@gmail.com>
2024-05-30 16:12:44 -0700

    added

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,17 +1,17 @@
 export default class Airport {
-    constructor(name, code) {
-        if (Object.getPrototypeOf(name) !== String.prototype) {
-            throw TypeError('Name must be a string');
-        }
-        if (Object.getPrototypeOf(code) !== String.prototype) {
-            throw TypeError('Code must be a string');
-        }
-        this._name = name;
-        this._code = code;
+  constructor(name, code) {
+    if (Object.getPrototypeOf(name) !== String.prototype) {
+      throw TypeError('Name must be a string');
     }
+    if (Object.getPrototypeOf(code) !== String.prototype) {
+      throw TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
 
-    // Method
-    get [Symbol.toStringTag]() {
-        return `${this._code}`;
-    }
+  // Method
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
 }
