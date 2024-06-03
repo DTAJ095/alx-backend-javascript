5e7d72ff39e8bf41ba45222ebb679e786004eba3
DTAJ095 <albanjaures26@gmail.com>
2024-06-03 16:42:32 -0700

    added

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 export default function cleanSet(set, startString) {
-    const segmt = [];
-    if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') return '';
-    for (const val of set.values()) {
-        if (typeof val === 'string' && val.startsWith(startString)) {
-            const subString = val.substring(startString.length);
-            if (subString && subString !== val) {
-                segmt.push(subString);
-            }
-        }
+  const segmt = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') return '';
+  for (const val of set.values()) {
+    if (typeof val === 'string' && val.startsWith(startString)) {
+      const subString = val.substring(startString.length);
+      if (subString && subString !== val) {
+        segmt.push(subString);
+      }
     }
-    return segmt.join('-');
+  }
+  return segmt.join('-');
 }
