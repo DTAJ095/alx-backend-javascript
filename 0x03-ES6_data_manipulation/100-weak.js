8b0ffbafa405df8c53d5c93d328121c658c9c4ad
DTAJ095 <albanjaures26@gmail.com>
2024-06-04 01:01:14 +0100

    added

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+/* global WeakMap */
+export const weakMap = new WeakMap();
+
+/* Maximum calls to make to an API's endpoint */
+const MAX_CALLS = 5;
+
+/* Tracks the number of calls made to an API's endpoint */
+export default function queryAPI(endpoint) {
+    if (weakMap.get(endpoint)) {
+        weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    } else {
+        weakMap.set(endpoint, 0);
+    }
+    if (weakMap.get(endpoint) >= MAX_CALLS) {
+        throw new Error('Endpoint load is high');
+    }
+}
