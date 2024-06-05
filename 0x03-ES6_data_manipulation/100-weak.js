1531f955b217a6257e51c79be99651ed69095e2d
DTAJ095 <albanjaures26@gmail.com>
2024-06-05 06:58:27 +0100

    adde

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,19 @@
+/**
+ * weak map of calls
+ */
 export const weakMap = new WeakMap();
 
 /* Maximum calls to make to an API's endpoint */
 const MAX_CALLS = 5;
 
-/* Tracks the number of calls made to an API's endpoint */
-export default function queryAPI(endpoint) {
+/**
+ * Tracks the number of calls made to an API's endpoint
+ * @param {{
+ *   protocol: string,
+ *   name: string,
+ * }} endpoint - the endpoint where the call is made
+ */
+export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   } weakMap.set(endpoint, 0);
