f053825b8a078c79b4bc0ba10621f7ff9691e36c
DTAJ095 <albanjaures26@gmail.com>
2024-07-25 16:40:09 -0700

    updated

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -4,35 +4,34 @@ const app = express();
 const PORT = 7865;
 
 app.get('/', (_, response) => {
-    response.send('Welcome to the payment system');
+  response.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (request, response) => {
-    const id = request.params.id;
+  const id = request.params.id;
 
-    response.send(`Payment methods for cart :${id}`);
+  response.send(`Payment methods for cart :${id}`);
 });
 
 app.post('/login', (request, response) => {
-    let userName = '';
-
-    if (request.body) {
-        userName = request.body.userName;
-    }
-    response.send(`Welcome :${userName}`);
+  let username = '';
+  if (request.body) {
+	    username = request.body.userName;
+  }
+  response.send(`Welcome ${username}`);
 });
 
 app.get('/available_payments', (_request, response) => {
-    response.send({
-        payment_methods: {
-            credit_cards: true,
-            paypal: false
-        }
-    });
+  response.send({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`API available on localhost port ${PORT}`);
+  console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
