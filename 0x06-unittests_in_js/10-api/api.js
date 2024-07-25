49a308826c1d62354671a7711bf3f357d37402f3
DTAJ095 <albanjaures26@gmail.com>
2024-07-25 22:13:42 +0100

    Doing test in js

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.get('/', (_, response) => {
+    response.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (request, response) => {
+    const id = request.params.id;
+
+    response.send(`Payment methods for cart :${id}`);
+});
+
+app.post('/login', (request, response) => {
+    let userName = '';
+
+    if (request.body) {
+        userName = request.body.userName;
+    }
+    response.send(`Welcome :${userName}`);
+});
+
+app.get('/available_payments', (_request, response) => {
+    response.send({
+        payment_methods: {
+            credit_cards: true,
+            paypal: false
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = app;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,57 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API Integration test', () => {
+  const URL_API = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${URL_API}/`, (_err, response, body) => {
+      expect(response.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${URL_API}/cart/101`, (_err, response, body) => {
+      expect(response.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 101');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
+    request.get(`${URL_API}/cart/-101`, (_err, response, _body) => {
+      expect(response.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, response, _body) => {
+      expect(response.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /available_payments returns valid response', (done) => {
+    request.get(`${URL_API}/available_payments`, (_err, response, body) => {
+        expect(response.statusCode).to.be.equal(200);
+        expect(JSON.parse(body)).to.be.equal({
+            payment_methods: {
+                credit_cards: true,
+                paypal: false
+            }
+        });
+        done();
+    });
+  });
+
+  it('POST /login returns valid response', (done) => {
+    request.post(`${URL_API}/login`, {json: {userName: 'John'}}, (_err, response, body) => {
+        expect(response.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Welcome John');
+        done();
+    });
+  });
+});
