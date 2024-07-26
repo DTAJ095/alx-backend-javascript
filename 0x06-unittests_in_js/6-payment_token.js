f4f686c41b1214b071d17bb535e2940e9f93b11f
DTAJ095 <albanjaures26@gmail.com>
2024-07-26 00:59:14 -0700

    updated

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,14 +1,7 @@
-// const getPaymentTokenFromAPI = (success) => new Promise((resolve, reject) => {
-//   if (success) {
-//     resolve({ data: 'Successful response from the API' });
-//   }
-// });
-function getPaymentTokenFromAPI(success) {
-  new Promise((resolve) => {
-    if (success) {
-      resolve({ data: 'Successful response from the API' })
-    }
-  })
-}
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, reject) => {
+  if (success) {
+    resolve({ data: 'Successful response from the API' });
+  }
+});
 
 module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,8 +4,8 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 describe('getPaymentTokenFromApi', () => {
   it('getPaymentTokenFromApi(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true).then((response) => {
-      expect(response).to.be.equal({ data: 'Successful response from the API' });
-      (done);
+      expect(response).to.deep.equal({ data: 'Successful response from the API' });
+      done();
     });
   });
 });
