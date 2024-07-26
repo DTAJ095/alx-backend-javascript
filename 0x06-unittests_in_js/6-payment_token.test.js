385cf67da87ffd47d6aecb54d30c457c8a2ebe93
DTAJ095 <albanjaures26@gmail.com>
2024-07-25 17:52:41 -0700

    updated

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,10 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromApi', () => {
-    it('getPaymentTokenFromApi(success), where success == true', (done) => {
-        getPaymentTokenFromAPI(true).then((response) => {
-            expect(response).to.be.equal({data: 'Successful response from the API' });
-            (done);
-        });
+  it('getPaymentTokenFromApi(success), where success == true', (done) => {
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response).to.be.equal({ data: 'Successful response from the API' });
+      (done);
     });
+  });
 });
